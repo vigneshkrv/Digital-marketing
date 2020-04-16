528d61c62f7db903866b1f373695afd715f2e4fa
Vignesh Kumar K R <42761544+vigneshkrv@users.noreply.github.com>
2020-04-15 22:48:38 -0500

    updated

--- backend/convert.js
+++ backend/convert.js
@@ -1,11 +1,18 @@
 const fs = require('fs')
+const fs2 = require('fs')
 const csv2json = require('csvjson-csv2json');
 
-var csvFile = fs.readFileSync('./dataSource/1429_1.csv').toString()
-
+var csvFile = fs.readFileSync('./dataSource/products.csv').toString()
+var csvFile2 = fs2.readFileSync('./dataSource/productReviews.csv').toString()
 const json = csv2json(csvFile, {parseNumbers: true});
+const json2 = csv2json(csvFile2, {parseNumbers: true});
+fs.writeFile('./dataSource/products.json',JSON.stringify(json),function(err) {
+    if(err) {
+        console.log(err)
+    }
+})
 
-fs.writeFile('./dataSource/1429_1.json',JSON.stringify(json),function(err) {
+fs.writeFile('./dataSource/productReviews.json',JSON.stringify(json2),function(err) {
     if(err) {
         console.log(err)
     }
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const { Client } = require('pg');
 
-const data = require('./dataSource/1429_1.json')
+const data = require('./dataSource/products.json')
+const review = require('./dataSource/productReviews.json')
 const express = require("express")
 const bodyParser = require("body-parser")
 const cors = require("cors")
@@ -10,52 +11,53 @@ var conn_string = "pg://postgres:root@127.0.0.1:5432/postgres";
 var conn = new Client(conn_string)
 
 
-conn.connect((err) => console.log(err))
+// conn.connect((err) => console.log(err))
 
-// conn.query("CREATE DATABASE AMAZON_REVIEWS")
+// conn.query("CREATE DATABASE product").catch((err) => {
+//     console.log()
+// }).finally(() => {
 
-conn.end()
+//     console.log("de")
+//    // conn.end()
 
-conn_string = "pg://postgres:root@127.0.0.1:5432/amazon_reviews";
+//     conn_string = "pg://postgres:root@127.0.0.1:5432/product";
 
-conn = new Client(conn_string)
+//     conn = new Client(conn_string)
 
-conn.connect((err) => console.log(err))
+//     conn.connect((err) => console.log(err))
 
-conn.query(`CREATE TABLE public.product_reviews
-(
-    id text COLLATE pg_catalog."default",
-    name text COLLATE pg_catalog."default",
-    asins text COLLATE pg_catalog."default",
-    brand text COLLATE pg_catalog."default",
-    categories text COLLATE pg_catalog."default",
-    manufacturer text COLLATE pg_catalog."default",
-    reviews_date text,
-    reviews_didpurchase text,
-    reviews_dorecommend boolean,
-    reviews_id text COLLATE pg_catalog."default",
-    reviews_numhelpful integer,
-    reviews_rating double precision,
-    reviews_sourceurls text COLLATE pg_catalog."default",
-    reviews_text text COLLATE pg_catalog."default",
-    reviews_title text COLLATE pg_catalog."default",
-    reviews_usercity text COLLATE pg_catalog."default",
-    reviews_username text COLLATE pg_catalog."default"
-)
-`)
+//     conn.query(`CREATE TABLE public.product_data
+//     (
+//     asin text COLLATE pg_catalog."default",
+//     brand text COLLATE pg_catalog."default",
+//     title text COLLATE pg_catalog."default",
+//     url text COLLATE pg_catalog."default",
+//     image text,
+//     rating double precision,
+//     reviewUrl text COLLATE pg_catalog."default",
+//     totalReviews integer,
+//     price double precision,
+//     originalPrice double precision
+//     )
+// `).catch(err => {console.log(err)})
+// })
 
-// var query = `INSERT INTO public.product_reviews(
-//     id, name, asins, brand, categories, manufacturer, reviews_date, reviews_didpurchase, reviews_dorecommend, reviews_id, reviews_numhelpful, reviews_rating, reviews_sourceurls, reviews_text, reviews_title, reviews_usercity, reviews_username)
-//     VALUES ('${element.id}', '${element.name}', '${element.asins}', '${element.brand}', '${element.categories}','${element.manufacturer}','${element["reviews.date"]}','${element["reviews.didPurchase"]}','${element["reviews.doRecommend"]}', '${element["reviews.id"]}', '${element["reviews.numHelpful"]}', '${element["reviews.rating"]}', '${element["reviews.sourceURLs"]}', '${element["reviews.text"]}', '${element["reviews.title"]}', '${element["reviews.userCity"]}', '${element["reviews.username"]}');`;
-// console.log(query)
+// // conn.end()
 
-data.forEach(element => {
-    var query = `INSERT INTO public.product_reviews(
-        id, name, asins, brand, categories, manufacturer, reviews_date, reviews_didpurchase, reviews_dorecommend, reviews_id, reviews_numhelpful, reviews_rating, reviews_sourceurls, reviews_text, reviews_title, reviews_usercity, reviews_username)
-        VALUES ('${element.id}', '${element.name}', '${element.asins}', '${element.brand}', '${element.categories}','${element.manufacturer}','${element["reviews.date"]}','${element["reviews.didPurchase"]}','${element["reviews.doRecommend"]}', '${element["reviews.id"]}', '${element["reviews.numHelpful"]}', '${element["reviews.rating"]}', '${element["reviews.sourceURLs"]}', '${element["reviews.text"]}', '${element["reviews.title"]}', '${element["reviews.userCity"]}', '${element["reviews.username"]}');`;
-    //console.log(query)
-    conn.query(query).catch(err => console.log())
-});
+
+
+// // var query = `INSERT INTO public.product_reviews(
+// //     id, name, asins, brand, categories, manufacturer, reviews_date, reviews_didpurchase, reviews_dorecommend, reviews_id, reviews_numhelpful, reviews_rating, reviews_sourceurls, reviews_text, reviews_title, reviews_usercity, reviews_username)
+// //     VALUES ('${element.id}', '${element.name}', '${element.asins}', '${element.brand}', '${element.categories}','${element.manufacturer}','${element["reviews.date"]}','${element["reviews.didPurchase"]}','${element["reviews.doRecommend"]}', '${element["reviews.id"]}', '${element["reviews.numHelpful"]}', '${element["reviews.rating"]}', '${element["reviews.sourceURLs"]}', '${element["reviews.text"]}', '${element["reviews.title"]}', '${element["reviews.userCity"]}', '${element["reviews.username"]}');`;
+// // console.log(query)
+
+// data.forEach(element => {
+//     var query = `INSERT INTO public.product_data(
+//         asin, brand, title, url, image, rating,reviewUrl, totalReviews,price,originalPrice)
+//         VALUES ('${element.asin}', '${element.brand}', '${element.title}', '${element.url}', '${element.image}','${element.rating}','${element.reviewUrl}','${element.totalReviews}','${element.price}', '${element.originalPrice}');`;
+//    // console.log(query)
+//     conn.query(query).catch(err => {console.log(err)})
+// });
 
 const app = express()
 app.use(bodyParser.urlencoded({
@@ -67,6 +69,10 @@ app.use(cors())
 var router = express.Router()
 router.route('/dashboard').get(function (req, res) {
     res.json(data)
+})
+
+router.route('/reviews').get(function (req,res) {
+    res.json(review)
 })
 
 app.use("/api", router)
